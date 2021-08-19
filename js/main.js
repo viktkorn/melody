b061a3ddf60beb2cf762d50301938066f6aa7dc3
Viktoria Kornienko <89094100+viktkorn@users.noreply.github.com>
2021-08-19 20:36:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-$(function(){
-$('.slider__box').slick({
-	prevArrow:'<img class="slider__arrow slider__arrow-left" src="images/arrow_left.svg" alt="arrow_left">',
-	nextArrow:'<img class="slider__arrow slider__arrow-right" src="images/arrow_right.svg" alt="arrow_right">',
-	responsive: [
-	{
-		breakpoint:481,
-		settings:{
-			arrows:false
+$(document).ready(function () {
+	var currentFloor = 2; //переменная где хранится текущий этаж
+	var floorPath = $(".home-image path"); //каждый отдельный этаж в SVG
+	var counterUp = $(".counter-up"); /*кнопка увеличения этажа */
+	var counterDown = $(".counter-down"); /*кнопка уменьшения этажа */
+	
+	//функция при наведении мышью на этаж
+	floorPath.on('mouseover', function(){
+		floorPath.removeClass('current-floor'); //удаляем активный класс у этажей
+		currentFloor = $(this).attr('data-floor'); //получаем значение текущего этажа
+		$(".counter").text(currentFloor); //записываем значение этажа в счетчик справа
+	});
+	counterUp.on("click",function(){ //отслеживаем клик по кнопке вверх
+		if(currentFloor < 18){ //проверяем значение этажа, не должно быть больше 18
+			currentFloor++; //прибавляем 1 этаж
+			//форматируем переменную с этажом, чтобы было 01, а не 1
+			usCurrentFloor = currentFloor.toLocaleString('en-US',{minimumIntegerDigits: 2, useGrouping:false}); 
+			$(".counter").text(usCurrentFloor); //записываем значение этажа в счетчик справа
+			floorPath.removeClass('current-floor'); //удаляем активный класс у этажей
+			$(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); //подсвечивем текущий этаж
 		}
-	}]
-});
-
-$('.menu-btn').on('click', function(){
-$('.menu__list').toggleClass('active');
-});
+	});
+	counterDown.on("click",function(){ //отслеживаем клик по кнопке вниз
+		if(currentFloor > 2){ //проверяем значение этажа, не должно быть меньше 2
+			currentFloor--; //отнимаем 1 этаж
+			//форматируем переменную с этажом, чтобы было 01, а не 1
+			usCurrentFloor = currentFloor.toLocaleString('en-US',{minimumIntegerDigits: 2, useGrouping:false});
+			$(".counter").text(usCurrentFloor); //записываем значение этажа в счетчик справа
+			floorPath.removeClass('current-floor'); //удаляем активный класс у этажей
+			$(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); //подсвечивем текущий этаж
+		}
+	});
 });
